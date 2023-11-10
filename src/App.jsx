f4bd4447e7546da4fed6ae9bf88523e1dd8f79bf
Praveen-D-0 <jopraveen06@gmail.com>
2023-11-10 12:02:03 +0530

    adding dynamic content to component

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,16 @@
- 
+function App() {
+  const now = new Date();
+  const a = 10;
+  const b = 29;
+  return (
+    <div>
+      <p>Hello World, it is {now.toString() }</p>
+      <p>
+        {a} plus {b} is {a + b}
+      </p>
+    </div>
+  )
+
+}
+
+export default App;
